ed9f5b4eb0704b36f0e771d4fedb8f264c52c34b
kite707 <kite707707@gmail.com>
2023-03-20 15:51:18 +0900

    fix: hardhat.config.js에 dotenv 적용

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require("dotenv");
+dotenv.config();
 module.exports = {
   networks: {
-    goeril: {
-      url: "https://ethereum-goerli-rpc.allthatnode.com/[Your API KEY]",
+    goerli: {
+      url:
+        "https://ethereum-goerli-rpc.allthatnode.com/" +
+        process.env.ALL_THAT_NODE_API_KEY,
+      accounts: [process.env.METAMASK_PRIVATE_KEY],
     },
   },
   solidity: "0.8.8",
