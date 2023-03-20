02561c94a83eed87b9970c464271f21aa4ce14f3
kite707 <kite707707@gmail.com>
2023-03-20 15:17:47 +0900

    feat: hardhat.config.js에 goeril RPC 연결 코드 추가

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,10 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-waffle");
 module.exports = {
+  networks: {
+    goeril: {
+      url: "https://ethereum-goerli-rpc.allthatnode.com/[Your API KEY]",
+    },
+  },
   solidity: "0.8.8",
 };
